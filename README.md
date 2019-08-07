239b6c2c42cbb66d317255c1ad0d04d989efb56f
Richard.Chen.Jing <783549412@qq.com>
2019-08-07 09:22:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ardunio ultrasonic ranging
+----
+**注意：** 本代码及论文所有权利归开发者所有。
+**Error：** All rights in this code and paper belong to the developer.
---
+++ demo1.ino
@@ -0,0 +1,59 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);//设置接口
+int light = 6;
+//int time = 60;
+int echo = 3;
+int trig = 2;
+int warn = 5;
+int jidianqi = 4;
+int t = 50;
+void setup()
+{
+	pinMode(trig,OUTPUT);
+	pinMode(echo, INPUT);
+	pinMode(warn, OUTPUT);
+	pinMode(jidianqi, OUTPUT);
+	lcd.begin(16, 2);
+	lcd.clear(); //清屏
+	delay(1000); //延时1000ms
+}
+
+void loop()
+{
+	digitalWrite(trig, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trig, HIGH);
+	delayMicroseconds(2);
+	digitalWrite(trig, LOW);
+	float timelengh = float(pulseIn(echo, HIGH));
+	float lengh = (timelengh * 17) / 1000;
+	analogWrite(light, 110);
+	//lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("lenght = ");
+	lcd.print(lengh);
+	lcd.setCursor(14, 0);
+	lcd.print("cm");
+        lcd.setCursor(0,1);
+        lcd.print("Warn distance:");
+        lcd.print(t);
+	if (lengh <= t)
+	{
+		if (lengh <= (t-20))
+		{
+			digitalWrite(jidianqi, HIGH);
+		}
+			for (int x = 0; x < 4; x++)
+			{
+				digitalWrite(warn, HIGH);
+				delay(lengh*3);
+				digitalWrite(warn, LOW);
+                                delay(lengh*3);
+
+			}
+		
+	}
+	delay(150);
+
+
+}
